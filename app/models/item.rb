1b70c98aa9f275a9460b5a030b678be6af213052
ryouma21 <zuotenglengzhen58@gmail.com>
2025-11-13 16:04:03 +0900

    Itemモデルに商品出品のバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,23 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_day
+   # 必須項目
+  validates :name,          presence: true
+  validates :description,   presence: true
+  validates :price,         presence: true
+   # 画像
+  validates :image,         presence: true
+ # ActiveHashの必須チェック（---=1を選ばせない）
+  validates :category_id,     numericality: { other_than: 1 }
+  validates :condition_id,    numericality: { other_than: 1 }
+  validates :shipping_fee_id, numericality: { other_than: 1 }
+  validates :prefecture_id,   numericality: { other_than: 1 }
+  validates :shipping_day_id, numericality: { other_than: 1 }
+   # 価格の制限
+  validates :price,
+  numericality: { 
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999
+  }
 end
