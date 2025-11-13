b3ff0ed2103f1ab440efeb06ab06e6fae81335ca
ryouma21 <zuotenglengzhen58@gmail.com>
2025-11-13 15:44:53 +0900

    ActiveHashの実装（カテゴリ・状態・送料負担・地域・発送日の追加）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
