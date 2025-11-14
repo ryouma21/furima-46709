673843f9a490465080fd5841f7392226169f2b43
ryouma21 <zuotenglengzhen58@gmail.com>
2025-11-14 16:37:17 +0900

    デバックコードの削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,12 +3,11 @@ const price = () => {
   if (!priceInput) return;
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
 
     const addTaxDom = document.getElementById("add-tax-price");
     const profitDom = document.getElementById("profit");
 
-    const tax = Math.floor(inputValue * 0.1);       
+    const tax = Math.floor(inputValue * 0.1);
     const profit = inputValue - tax; 
 
     addTaxDom.innerHTML = tax;
