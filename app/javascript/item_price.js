5804ccb6bf858d6d49531ab271d2446317604490
ryouma21 <zuotenglengzhen58@gmail.com>
2025-11-14 12:10:22 +0900

    価格計算のJavaScriptを実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,24 @@
+const price = () => {
+  const priceInput = document.getElementById("item-price");
+  if (!priceInput) return;
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value; // 入力された値を取得
+    console.log(inputValue);             // 画面に出して確認！
+
+    // 表示する場所の要素を取得
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+
+    // 計算（10%の手数料）
+    const tax = Math.floor(inputValue * 0.1);       
+    const profit = inputValue - tax; 
+
+    // HTMLを書き換える
+    addTaxDom.innerHTML = tax;
+    profitDom.innerHTML = profit;
+
+  })
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
