d60a895d59366aa437b38777fb6b6c548dc1e695
ryouma21 <zuotenglengzhen58@gmail.com>
2025-11-14 15:52:39 +0900

    itemモデルのテストコードを@itemに統一し、動作するよう修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -21,69 +21,69 @@ RSpec.describe Item, type: :model do
     end
 
     it '商品の説明が空では出品できない' do
-      item = FactoryBot.build(:item, description: '')
-      item.valid?
-      expect(item.errors.full_messages).to include("Description can't be blank")
+      @item.description = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
     end
 
     it '価格が空では出品できない' do
-      item = FactoryBot.build(:item, price: '')
-      item.valid?
-      expect(item.errors.full_messages).to include("Price can't be blank")
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'カテゴリーが --- では出品できない' do
-      item = FactoryBot.build(:item, category_id: 1)
-      item.valid?
-      expect(item.errors.full_messages).to include('Category must be other than 1')
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Category must be other than 1')
     end
 
     it '商品の状態が --- では出品できない' do
-      item = FactoryBot.build(:item, condition_id: 1)
-      item.valid?
-      expect(item.errors.full_messages).to include('Condition must be other than 1')
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Condition must be other than 1')
     end
 
     it '配送料の負担が --- では出品できない' do
-      item = FactoryBot.build(:item, shipping_fee_id: 1)
-      item.valid?
-      expect(item.errors.full_messages).to include('Shipping fee must be other than 1')
+      @item.shipping_fee_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Shipping fee must be other than 1')
     end
 
     it '発送元の地域が --- では出品できない' do
-      item = FactoryBot.build(:item, prefecture_id: 1)
-      item.valid?
-      expect(item.errors.full_messages).to include('Prefecture must be other than 1')
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
     end
 
     it '発送までの日数が --- では出品できない' do
-      item = FactoryBot.build(:item, shipping_day_id: 1)
-      item.valid?
-      expect(item.errors.full_messages).to include('Shipping day must be other than 1')
+      @item.shipping_day_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Shipping day must be other than 1')
     end
 
     it '価格が299円以下では出品できない' do
-      item = FactoryBot.build(:item, price: 299)
-      item.valid?
-      expect(item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
     end
 
     it '価格が10,000,000円以上では出品できない' do
-      item = FactoryBot.build(:item, price: 10_000_000)
-      item.valid?
-      expect(item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      @item.price = 10_000_000
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
     end
 
     it '価格が全角数字だと出品できない' do
-      item = FactoryBot.build(:item, price: '３００')
-      item.valid?
-      expect(item.errors.full_messages).to include('Price is not a number')
+      @item.price = '３００'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price is not a number')
     end
 
     it '価格が半角数字以外では出品できない' do
-      item = FactoryBot.build(:item, price: 'abc')
-      item.valid?
-      expect(item.errors.full_messages).to include('Price is not a number')
+      @item.price = 'abc'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price is not a number')
     end
   end
 end
