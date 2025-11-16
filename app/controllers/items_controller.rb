0afbcf772d97fbc1d989aa629408a2a085a5dd0f
ryouma21 <zuotenglengzhen58@gmail.com>
2025-11-16 12:11:55 +0900

    destroyアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,12 @@ class ItemsController < ApplicationController
   def edit
   end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+
   def update
     if @item.update(item_params)
       redirect_to item_path(@item)
