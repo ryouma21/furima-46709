bcfeedde16657d8744127517a3a8bc406d3e5dea
ryouma21 <zuotenglengzhen58@gmail.com>
2025-11-18 18:24:37 +0900

    ・売却済み商品の編集ページへのアクセスを制限し、意図しない操作を防ぐため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:edit, :update, :show, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :move_to_index_if_sold_out, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -57,4 +58,9 @@ class ItemsController < ApplicationController
 
     redirect_to root_path
   end
+
+  def move_to_index_if_sold_out
+    redirect_to root_path if @item.purchase.present?
+  end
+
 end
