fbe0a3f41194fd24ec9e383feadc25bf8a706dd3
ryouma21 <zuotenglengzhen58@gmail.com>
2025-11-18 20:44:07 +0900

    PurchaseAddress のバリデーションを with_options でリファクタリング

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -5,11 +5,10 @@ class PurchaseAddress
                 :postal_code, :prefecture_id, :city, :house_number,
                 :building_name, :phone_number, :token
 
-  # ▼ バリデーション
-
-  validates :token, presence: true                      # ① Token
-  validates :postal_code, presence: true                # ② 郵便番号
-
+  with_options presence: true do
+    validates :token, presence: true                      # ① Token
+    validates :postal_code, presence: true                # ② 郵便番号
+  end
   # 郵便番号（3桁-4桁）
   validates :postal_code, format: {
     with: /\A\d{3}-\d{4}\z/,
@@ -19,12 +18,14 @@ class PurchaseAddress
   # 都道府県（---=1 を選ばせない）
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
-  validates :city, presence: true                   # ④ 市区町村
-  validates :house_number, presence: true           # ⑤ 番地
-  validates :phone_number, presence: true           # ⑥ 電話番号
-  validates :user_id, presence: true                # ⑦ user_id
-  validates :item_id, presence: true                # ⑧ item_id
-
+  with_options presence: true do
+    validates :city, presence: true                   # ④ 市区町村
+    validates :house_number, presence: true           # ⑤ 番地
+    validates :phone_number, presence: true           # ⑥ 電話番号
+    validates :user_id, presence: true                # ⑦ user_id
+    validates :item_id, presence: true                # ⑧ item_id
+  end
+  
   # 電話番号（10〜11桁）
   validates :phone_number, length: { minimum: 10, message: 'is too short' }
   validates :phone_number, format: {
