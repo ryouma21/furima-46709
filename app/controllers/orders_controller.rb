e72b6e3d3b45a4996b95e3e0f56ddec8a57832c9
ryouma21 <zuotenglengzhen58@gmail.com>
2025-11-18 13:49:30 +0900

    購入機能用のOrdersコントローラーとルーティングを追加

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,34 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  
+  def index
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def purchase_params
+    params.require(:purchase_address).permit(
+      :postal_code, :prefecture_id, :city,
+      :house_number, :building_name, :phone_number
+    ).merge(
+      user_id: current_user.id,
+      item_id: params[:item_id]
+    )
+  end
+end
